ebf4ccc8eeb7ed060364459f4e201adacc3443de
Nesfee <Nesfee1@mail.ru>
2023-10-01 16:49:38 +0300

    Add gitignore, fs-extra and fix main.js

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-x = 1;
-y = 2;
-c = x + y;
-console.log(c);
+const fs = require('fs-extra');
+fs.mkdirSync('Folder1');
+fs.writeFileSync('Folder1/text.txt', "Hello");
+fs.mkdirSync('Folder2');
+fs.moveSync('Folder1/text.txt', 'Folder2/text.txt');
+fs.mkdirSync('Folder3');
+fs.copyFileSync('Folder2/text.txt', 'Folder3/text.txt');
+fs.rmSync('Folder2/text.txt');
+fs.rmSync('Folder3/text.txt');
+fs.rmdirSync('Folder1');
+fs.rmdirSync('Folder2');
+fs.rmdirSync('Folder3')

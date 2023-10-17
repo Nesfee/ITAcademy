9b1b639ce01821c0162bfd180baf284082dc7b13
Nesfee <Nesfee1@mail.ru>
2023-10-17 19:18:10 +0300

    Fix 7.4

--- Homework7/HomeworkNumber7.4.js
+++ Homework7/HomeworkNumber7.4.js
@@ -1,40 +1,39 @@
-function SearchMatches(number1, number2) {
-    count1 = 0
-    count2 = 0
-    let array1 = [...number1.toString()].map(Number);
-    let array2 = [...number2.toString()].map(Number);
-    array1.forEach(function(item1, index1) {
-        array2.forEach(function(item2, index2) {
-            if (item1 == item2) {
-                count2 ++
-            }
-            if (item1 == item2 && index1 == index2) {
-                count1++
-            }
-        });
-      });
-      return [count1, count2].join(' ')
-}
+function countMatches(number1, number2) {
+    var number1String = String(number1);
+    var number2String = String(number2);
+    var matchPosition = 0;
+    var matchValue = 0;
+  
+    for (var i = 0; i < 4; i++) {
+      if (number1String[i] === number2String[i]) {
+        matchPosition++;
+      } else if (number2String.includes(number1String[i])) {
+        matchValue++;
+      }
+    }
+  
+    console.log(matchPosition, matchValue);
+  }
+  
+  countMatches(3487, 3794);
 
-console.log(SearchMatches(3487, 3794))
-
-// Через циклы
-// function SearchMatches(number1, number2) { 
-//     let array1 = [...number1.toString()].map(Number);
-//     let array2 = [...number2.toString()].map(Number);
+  
+  // function SearchMatches(number1, number2) {
 //     count1 = 0
 //     count2 = 0
-//     for(let i = 0; i < 4; i++) {
-//         if (array1[i] == array2[i]) {
-//             count1++
-//         }
-//         for(let n = 0; n < 4; n++) {
-//             if (array1[i] == array2[n]) {
-//                 count2++
+//     let array1 = [...number1.toString()].map(Number);
+//     let array2 = [...number2.toString()].map(Number);
+//     array1.forEach(function(item1, index1) {
+//         array2.forEach(function(item2, index2) {
+//             if (item1 == item2) {
+//                 count2 ++
 //             }
-//         }
-//     }
-//     return [count1, count2].join(' ')
+//             if (item1 == item2 && index1 == index2) {
+//                 count1++
+//             }
+//         });
+//       });
+//       return [count1, count2].join(' ')
 // }
 
 // console.log(SearchMatches(3487, 3794))

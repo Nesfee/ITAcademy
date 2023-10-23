ae90bb9aba28b829e4fbe2c4a81590657d813b5d
Nesfee <Nesfee1@mail.ru>
2023-10-23 18:13:56 +0300

    fix HomeworkNumber7.3

--- Homework7/HomeworkNumber7.3.js
+++ Homework7/HomeworkNumber7.3.js
@@ -1,5 +1,24 @@
-function getFibonachi(number) {
-    return number <= 1 ? number: getFibonachi(number - 1) + getFibonachi(number - 2);
+function getFibonachi(n, m) {
+    let fibonacciArr = [];
+
+    let prevNum = 0;
+    let nextNum = 1;
+
+    for (let i = 1; i <= n + m; i++) {
+    if (i >= n) {
+    fibonacciArr.push(prevNum);
+
+    if (fibonacciArr.length === m) {
+    break;
+    }
+    }
+
+    let temp = nextNum;
+    nextNum = prevNum + nextNum;
+    prevNum = temp;
+    }
+
+    return fibonacciArr;
 }
 
-console.log(getFibonachi(7))
+console.log(getFibonachi(5, 8))

d92da3332810af747ecf651ede7a936e076c37c6
Nesfee <Nesfee1@mail.ru>
2023-11-05 15:07:41 +0300

    new structure

---
+++ Homework5/HomeworkNumber5.js
@@ -0,0 +1,19 @@
+let str = "Hello"
+let num = 5
+let bol = true
+
+console.log(str + bol) // Сложение
+console.log(str + num)
+console.log(num + bol)
+
+console.log(num * bol) // Умножение
+console.log(str * num)
+console.log(num * bol)
+
+console.log(num / bol) // Деление
+console.log(str / num)
+console.log(num / bol)
+
+console.log(Number(str)) // Явное преобразование
+console.log(String(num))
+console.log(Boolean(str))

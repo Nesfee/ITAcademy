45754b8a02029bc4ab56897e7dfebde0d2a714bc
Nesfee <Nesfee1@mail.ru>
2023-11-20 19:20:37 +0300

    1 negative test

--- Homework16/tests/books.test.js
+++ Homework16/tests/books.test.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 const Validator = require('jsonschema').Validator;
-const bookAPIGet = require('../data/booksAPIschemaGet.json')
-const bookAPIOther = require('../data/booksAPIschemaOther.json')
+const bookAPIGet = require('../data/booksAPIschemaGet.json');
+const bookAPIOther = require('../data/booksAPIschemaOther.json');
 const validate = new Validator();
 
 describe('API positive tests', () => {
   test('Get all books', async () => {
     const response = await axios.get('https://fakerestapi.azurewebsites.net/api/v1/Books');
-    const result = validate.validate(response.data, bookAPIGet)
+    const result = validate.validate(response.data, bookAPIGet);
     expect(response.status).toBe(200);
     expect(result.valid).toBe(true);
   });
@@ -15,7 +15,7 @@ describe('API positive tests', () => {
   test('Get a specific book', async () => {
     const specificBookId = 1;
     const response = await axios.get(`https://fakerestapi.azurewebsites.net/api/v1/Books/${specificBookId}`);
-    const result = validate.validate(response.data, bookAPIOther)
+    const result = validate.validate(response.data, bookAPIOther);
     expect(response.status).toBe(200);
     expect(result.valid).toBe(true);
   });
@@ -30,7 +30,7 @@ describe('API positive tests', () => {
       "publishDate": "2023-11-19T16:33:57.660Z"
   };
     const response = await axios.post('https://fakerestapi.azurewebsites.net/api/v1/Books', newBook);
-    const result = validate.validate(response.data, bookAPIOther)
+    const result = validate.validate(response.data, bookAPIOther);
     expect(response.status).toBe(200);
     expect(result.valid).toBe(true);
   });
@@ -46,7 +46,7 @@ describe('API positive tests', () => {
       "publishDate": "2023-11-19T16:33:57.660Z"
     };
     const response = await axios.put(`https://fakerestapi.azurewebsites.net/api/v1/Books/${specificBookId}`, bookToUpdate);
-    const result = validate.validate(response.data, bookAPIOther)
+    const result = validate.validate(response.data, bookAPIOther);
     expect(response.status).toBe(200);
     expect(result.valid).toBe(true);
   });
@@ -57,3 +57,23 @@ describe('API positive tests', () => {
     expect(response.status).toBe(200);
   });
 })
+
+describe('API negative tests', () => {
+  test('Should return schema valid false', async () => {
+    const newBook = {
+      "id": 0,
+      "title": "string",
+      "description": "string",
+      "pageCount": 0,
+      "excerpt": "string",
+      "publishDate": "2023-11-19T16:33:57.660Z"
+  };
+    try {
+      response = await axios.post('https://fakerestapi.azurewebsites.net/api/v1/Books', newBook);
+    } catch (err) {
+        response = err;
+    }
+    const result = validate.validate(response.data, bookAPIGet);
+    expect(result.valid).toBe(false);
+  });
+});
